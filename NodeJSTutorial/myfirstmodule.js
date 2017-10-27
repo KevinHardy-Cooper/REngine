b1905ad87fe332800c40e3c16d8951ca4c639111
KevinHardy-Cooper <kevinhardycooper@gmail.com>
2017-10-26 22:12:25 -0400

    Let's get this party started! Working through the tutorial, at Update Files in https://www.w3schools.com/nodejs/nodejs_filesystem.asp

---
+++ NodeJSTutorial/myfirstmodule.js
@@ -0,0 +1,6 @@
+// my own module
+// returns a date and time object
+// exports makes properties and methods available outside the module file
+exports.myDateTime = function() {
+	return Date();
+};
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # REngine
 Recommendation engine utilizing in ReactJS and Node.js
+
+How to Install:
+1. Install Node.js on your machine
