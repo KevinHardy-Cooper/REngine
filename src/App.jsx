c947f7d2d87b89dde7c7ad03362ae80524e30a64
KevinHardy-Cooper <kevinhardycooper@gmail.com>
2017-11-05 16:37:11 -0500

    adding more fields to props, and random values

--- src/App.jsx
+++ src/App.jsx
@@ -50,131 +50,97 @@ class Request extends React.Component {
 class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: '', name: '', plan: '', servProv: '', totalDue: 0, startDate: '', endDate: '', longDistance: 0, textMsgSent: 0, textMsgReceived: 0, dataUsage: 0, localAirtime: 0
-   };
+    
+    // Personal Info
+    // This will change depending on user
+    this.name = 'Mr. T';
 
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handlePlanChange = this.handlePlanChange.bind(this);
-    this.handleServiceProviderChange = this.handleServiceProviderChange.bind(this);
-    this.handleTotalDueChange = this.handleTotalDueChange.bind(this);
-    this.handleStartDateChange = this.handleStartDateChange.bind(this);
-    this.handleEndDateChange = this.handleEndDateChange.bind(this);
-    this.handleLongDistanceChange = this.handleLongDistanceChange.bind(this);
-    this.handleTextMsgSentChange = this.handleTextMsgSentChange.bind(this);
-    this.handleTextMsgReceivedChange = this.handleTextMsgReceivedChange.bind(this);
-    this.handleDataUsageChange = this.handleDataUsageChange.bind(this);
-    this.handleLocalAirtimeChange = this.handleLocalAirtimeChange.bind(this);
+    // Plan related
+    // This will change depending on plan
+    this.plan = 'Canada-Wide Talk + Text 25 Dbl';
+    this.servProv = 'Koodo';
+
+    // Final charge
+    // This will change depending the invoice
+    this.totalDue = 25;
+    
+    // Billing cycle
+    // This will change depending on the billing cycle
+    var d = new Date();
+    this.startDate = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate();
+    this.endDate = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + (d.getDate() - 1) ;
+
+    // Allowances
+    // This will change depending on the plan
+    this.longDistanceAllowed = 0;
+    this.textMsgSentAllowed = Number.POSITIVE_INFINITY;
+    this.textMsgReceivedAllowed = Number.POSITIVE_INFINITY;
+    this.dataUsageAllowed = 0
+    this.localAirtimeAllowed = 200;
+
+    // Usages
+    // This will change depending on the usage
+    this.longDistance = Math.round(Math.random() * 50);
+    this.textMsgSent = Math.round(Math.random() * 4000);
+    this.textMsgReceived = Math.round(Math.random() * 4000);
+    this.dataUsage = Math.round(Math.random() * 10 * Math.pow(10, 2))/Math.pow(10,2);
+    this.localAirtime = Math.round(Math.random() * 500);
+
+    // Usage charges
+    // This will change depending on the usage and the charges
+    var potentialLongDistanceOverage = this.longDistance - this.longDistanceAllowed;
+    this.longDistanceCharge = potentialLongDistanceOverage > 0 ? potentialLongDistanceOverage * 0.50 : 0;
+    
+    var potentialTextMsgSentOverage = this.textMsgSent - this.textMsgSentAllowed;
+    this.textMsgSentCharge = this.textMsgSentAllowed != Number.POSITIVE_INFINITY ? potentialTextMsgSentOverage * 0.25 : 0;
+
+    var potentialTextMsgReceivedOverage = this.textMsgReceived - this.textMsgReceivedAllowed;
+    this.textMsgReceivedCharge = this.textMsgReceivedAllowed != Number.POSITIVE_INFINITY ? potentialTextMsgReceivedOverage * 0.25 : 0;
+    
+    var potentialDataUsageOverage = this.dataUsage - this.dataUsageAllowed;
+    this.dataUsageCharge = potentialDataUsageOverage > 0 ? potentialDataUsageOverage * 2 : 0;
+
+    var potentialLocalAirtimeOverage = this.localAirtime - this.localAirtimeAllowed;
+    this.localAirtimeCharge = potentialLocalAirtimeOverage > 0 ? potentialLocalAirtimeOverage * 0.5 : 0;
+
+    this.state = {value: ''};
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleNameChange(event) {
-    this.setState({name: event.target.value});
-  }
-
-  handlePlanChange(event) {
-    this.setState({plan: event.target.value});
-  }
-
-  handleServiceProviderChange(event) {
-    this.setState({servProv: event.target.value});
-  }
-
-  handleTotalDueChange(event) {
-    this.setState({totalDue: event.target.value});
-  }
-
-  handleStartDateChange(event) {
-    this.setState({startDate: event.target.value});
-  }
-
-  handleEndDateChange(event) {
-    this.setState({endDate: event.target.value});
-  }
-  handleLongDistanceChange(event) {
-    this.setState({longDistance: event.target.value});
-  }
-
-  handleTextMsgSentChange(event) {
-    this.setState({textMsgSent: event.target.value});
-  }
-
-  handleTextMsgReceivedChange(event) {
-    this.setState({textMsgReceived: event.target.value});
-  }
-
-  handleDataUsageChange(event) {
-    this.setState({dataUsage: event.target.value});
-  }
-
-  handleLocalAirtimeChange(event) {
-    this.setState({localAirtime: event.target.value});
-  }
-
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.name + '.\nA plan was submitted: ' + this.state.plan + '.\nA service provider was submitted: ' + this.state.servProv  + '.\nThe total due was submitted: ' + this.state.totalDue + '.\nThe start date was submitted: ' + this.state.startDate + '.\nThe end date was submitted: ' + this.state.endDate + '.\nThe long distance usage charge was submitted: ' + this.state.longDistance + '.\nThe text messages sent charge was submitted: ' + this.state.textMsgSent + '.\nThe text messages received charge was submitted: ' + this.state.textMsgReceived + '.\nThe local airtime usage charge was submitted: ' + this.state.localAirtime);
+    alert(  'A name was submitted: ' + this.name + '.\n'
+          + 'A plan was submitted: ' + this.plan + '.\n'
+          + 'A service provider was submitted: ' + this.servProv  + '.\n' 
+          + 'The total due was submitted: ' + this.totalDue + '.\n'
+          + 'The start date was submitted: ' + this.startDate + '.\n'
+          + 'The end date was submitted: ' + this.endDate + '.\n'
+          + 'The long distance usage charge was submitted: ' + this.longDistance + '.\n'
+          + 'The text messages sent charge was submitted: ' + this.textMsgSent + '.\n' 
+          + 'The text messages received charge was submitted: ' + this.textMsgReceived + '.\n' + 'The local airtime usage charge was submitted: ' + this.localAirtime);
     event.preventDefault();
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.name} onChange={this.handleNameChange} />
-        </label>
-        <br/>
-        <label>
-          Plan:
-          <input type="text" value={this.state.plan} onChange={this.handlePlanChange} />
-        </label>
-        <br/>
-        <label>
-          Service Provider:
-          <input type="text" value={this.state.servProv} onChange={this.handleServiceProviderChange} />
-        </label>
-        <br/>
-        <label>
-          Total Due:
-          <input type="number" value={this.state.totalDue} onChange={this.handleTotalDueChange} />
-        </label>
-        <br/>
-         <label>
-          Billing Cycle Start Date:
-          <input type="date" value={this.state.startDate} onChange={this.handleStartDateChange} />
-        </label>
-        <br/>
-         <label>
-          Billing Cycle End Date:
-          <input type="date" value={this.state.endDate} onChange={this.handleEndDateChange} />
-        </label>
-        <br/>
+        <p>Name: {this.name}</p>
+        <p>Plan: {this.plan}</p>
+        <p>Service Provider: {this.servProv}</p>
+        <p>Total Due: ${this.totalDue}</p>
+        <p>Billing Cycle Start Date: {this.startDate}</p>
+        <p>Billing Cycle End Date: {this.endDate}</p>
         <h3>Usage Charges:</h3>
-        <label>
-         Long Distance - Domestic Phone:
-          <input type="number" value={this.state.longDistance} onChange={this.handleLongDistanceChange} />
-        </label>
-        <br/>
-        <label>
-         Text Msg Sent:
-          <input type="number" value={this.state.textMsgSent} onChange={this.handleTextMsgSentChange} />
-        </label>
-        <br/>
-        <label>
-         Text Msg Received:
-          <input type="number" value={this.state.textMsgReceived} onChange={this.handleTextMsgReceivedChange} />
-        </label>
-        <br/>
-        <label>
-         Data Usage:
-          <input type="number" value={this.state.dataUsage} onChange={this.handleDataUsageChange} />
-        </label>
-        <br/>
-        <label>
-         Local Airtime:
-          <input type="number" value={this.state.localAirtime} onChange={this.handleLocalAirtimeChange} />
-        </label>
-        <br/>
-
+        <p>Long Distance - Domestic Phone (of {this.longDistanceAllowed}:00): {this.longDistance}:00</p>
+        <p><strong>Overage Charge for Long Distance: ${this.longDistanceCharge} @ $0.50 / overage minute of long distance airtime</strong></p>
+        <p>Text Msg Sent (of {this.textMsgSentAllowed}): {this.textMsgSent}</p>
+        <p><strong>Overage Charge for Text Msg Sent: ${this.textMsgSentCharge} @ $0.25 / overage text message sent</strong></p>
+        <p>Text Msg Received (of {this.textMsgReceivedAllowed}): {this.textMsgReceived}</p>
+        <p><strong>Overage Charge for Text Received Sent: ${this.textMsgReceivedCharge} @ $0.25 / overage text message received</strong></p>
+        <p>Data Usage (of {this.dataUsageAllowed} MB): {this.dataUsage} MB</p>
+        <p><strong>Overage Charge for Data: ${this.dataUsageCharge} @ $2.00 / overage MB of data used</strong></p>
+        <p>Local Airtime (of {this.localAirtimeAllowed}:00): {this.localAirtime}:00</p>
+        <p><strong>Overage Charge for Local Airtime: ${this.localAirtimeCharge} @ $0.5 / overage minute of local airtime</strong></p>
         <input type="submit" value="Submit" />
       </form>
     );
